91e216898b94a36ed0846cfd2334c74920a03bec
EzequielDiaz777 <diazezequiel777@gmail.com>
2024-05-19 01:24:01 -0300

    Vista de inmueble corregida

--- app/src/main/java/com/ezediaz/inmobiliaria/MainActivity.java
+++ app/src/main/java/com/ezediaz/inmobiliaria/MainActivity.java
@@ -29,18 +29,10 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null)
-                        .setAnchorView(R.id.fab).show();
-            }
-        });
+
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_mapa, R.id.nav_perfil, R.id.nav_lista, R.id.nav_salir)
                 .setOpenableLayout(drawer)
@@ -63,31 +55,4 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
-
-    /*
-    FATAL EXCEPTION: main
-    Process: com.ezediaz.myapplication, PID: 20114
-    java.lang.RuntimeException: Unable to start activity ComponentInfo{com.ezediaz.myapplication/com.ezediaz.myapplication.MainActivity}: java.lang.IllegalStateException: This Activity already has an action bar supplied by the window decor. Do not request Window.FEATURE_SUPPORT_ACTION_BAR and set windowActionBar to false in your theme to use a Toolbar instead.
-    at android.app.ActivityThread.performLaunchActivity(ActivityThread.java:3491)
-    at android.app.ActivityThread.handleLaunchActivity(ActivityThread.java:3659)
-    at android.app.servertransaction.LaunchActivityItem.execute(LaunchActivityItem.java:85)
-    at android.app.servertransaction.TransactionExecutor.executeCallbacks(TransactionExecutor.java:135)
-    at android.app.servertransaction.TransactionExecutor.execute(TransactionExecutor.java:95)
-    at android.app.ActivityThread$H.handleMessage(ActivityThread.java:2115)
-    at android.os.Handler.dispatchMessage(Handler.java:106)
-    at android.os.Looper.loop(Looper.java:250)
-    at android.app.ActivityThread.main(ActivityThread.java:7755)
-    at java.lang.reflect.Method.invoke(Native Method)
-    at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:592)
-    at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:958)
-    Caused by: java.lang.IllegalStateException: This Activity already has an action bar supplied by the window decor. Do not request Window.FEATURE_SUPPORT_ACTION_BAR and set windowActionBar to false in your theme to use a Toolbar instead.
-    at androidx.appcompat.app.AppCompatDelegateImpl.setSupportActionBar(AppCompatDelegateImpl.java:630)
-    at androidx.appcompat.app.AppCompatActivity.setSupportActionBar(AppCompatActivity.java:185)
-    at com.ezediaz.myapplication.MainActivity.onCreate(MainActivity.java:35)
-    at android.app.Activity.performCreate(Activity.java:8077)
-    at android.app.Activity.performCreate(Activity.java:8061)
-    at android.app.Instrumentation.callActivityOnCreate(Instrumentation.java:1309)
-    at android.app.ActivityThread.performLaunchActivity(ActivityThread.java:3464)
-     */
-
 }
